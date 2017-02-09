1d7c8252feacc663afcad360a21c089ac8c33c77
svey <marcussvehlak.code@gmail.com>
2017-02-08 20:18:08 -0800

    Added react-router o Please enter the commit message for your changes. Lines starting

--- public/index.js
+++ public/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router';
 import App from './client/components/App.js';
 import Map from './client/components/map.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<App />, document.getElementById('app'));
+
+render(
+    <Router history={browserHistory}>
+        <Route path="/" component={App}/>
+    </Router>,
+    document.getElementById('app')
+);
